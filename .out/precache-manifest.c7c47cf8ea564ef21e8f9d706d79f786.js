9abb94c87d70d0e5f05af3eb777c3b427e6ea02e
John <jjiang@fusionlabs.com.au>
2020-05-23 01:49:51 +1000

    Added storybook build

---
+++ .out/precache-manifest.c7c47cf8ea564ef21e8f9d706d79f786.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c9fe0cd14fe750b307bd69391dd7a9",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ae9b28188157e5d07e1d.bundle.js"
+  },
+  {
+    "url": "runtime~main.ae9b28188157e5d07e1d.bundle.js"
+  },
+  {
+    "url": "vendors~main.ae9b28188157e5d07e1d.bundle.js"
+  },
+  {
+    "url": "vendors~main.ae9b28188157e5d07e1d.bundle.js.LICENSE.txt"
+  }
+]);
